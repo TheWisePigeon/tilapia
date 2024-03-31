988a35adac3c7b0144002804c1198f7e94d8753d
TheWisePigeon <zozozozeph@gmail.com>
2024-03-31 16:07:20 +0000

    ded simple

--- main.go
+++ main.go
@@ -4,69 +4,31 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
-
-type Token string
-
-const (
-	I Token = "i"
-	D Token = "d"
-	S Token = "s"
-	O Token = "o"
-)
-
-var accumulator int = 0
-var tokens []Token
-
-func tokenize(input string) {
-	input = strings.TrimSuffix(input, "\n")
-	for pos, r := range input {
-		token := Token(r)
-		switch token {
-		case I, D, S, O:
-			tokens = append(tokens, token)
-		default:
-			fmt.Printf("Illegal character: %s encountered in input at position %d\n", token, pos+1)
-			tokens = []Token{}
-		}
-	}
-}
-
-func resetAccumulator() {
-	if accumulator == -1 || accumulator == 256 {
-		accumulator = 0
-	}
-}
-
-func interpret() {
-	for _, token := range tokens {
-		switch token {
-		case I:
-			accumulator = accumulator + 1
-			resetAccumulator()
-		case D:
-			accumulator = accumulator - 1
-			resetAccumulator()
-		case S:
-			accumulator = accumulator * accumulator
-			resetAccumulator()
-		case O:
-			fmt.Println(accumulator)
-		default:
-			panic(fmt.Sprintf("Invalid token %s", token))
-		}
-	}
-	tokens = []Token{}
-}
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
+	var accumulator int
 	for {
 		fmt.Print(">> ")
-		scanner.Scan()
-		input := scanner.Text()
-		tokenize(input)
-		interpret()
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			return
+		}
+		for _, c := range input {
+			switch c {
+			case 's':
+				accumulator = accumulator * accumulator
+			case 'i':
+				accumulator++
+			case 'd':
+				accumulator--
+			case 'o':
+				fmt.Println(accumulator)
+			}
+			if accumulator < 0 || accumulator == 256 {
+				accumulator = 0
+			}
+		}
 	}
 }
